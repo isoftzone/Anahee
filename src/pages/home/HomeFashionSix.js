bee5391cb7911a2b52b7d5fdbe9c1e5419f37489
vinod <p.vinod@isoftzone.com>
2025-01-20 14:20:01 +0530

    Initial commit

---
+++ src/pages/home/HomeFashionSix.js
@@ -0,0 +1,74 @@
+import React, { Fragment } from "react";
+import SEO from "../../components/seo";
+import LayoutSeven from "../../layouts/LayoutSeven";
+import HeroSliderFourteen from "../../wrappers/hero-slider/HeroSliderFourteen";
+import SectionTitleWithText from "../../components/section-title/SectionTitleWithText";
+import FeatureIconFour from "../../wrappers/feature-icon/FeatureIconFour";
+import TabProductEight from "../../wrappers/product/TabProductEight";
+import NewsletterTwo from "../../wrappers/newsletter/NewsletterTwo";
+import ImageSliderOne from "../../wrappers/image-slider/ImageSliderOne";
+import TabProductTwelve from "../../wrappers/product/TabProductTwelve";
+import BannerNine from "../../wrappers/banner/BannerNine";
+import TabProductFive from "../../wrappers/product/TabProductFive";
+import BannerFifteen from "../../wrappers/banner/BannerFifteen";
+import BannerSixteen from "../../wrappers/banner/BannerSixteen";
+import ProductGridFiveContainer from "../../wrappers/product/ProductGridFiveContainer";
+
+const HomeFashionSix = () => {
+  return (
+    <Fragment>
+      <SEO
+        titleTemplate="Fashion Home"
+        description="Fashion home of flone react minimalist eCommerce template."
+      />
+      <LayoutSeven>
+        {/* hero slider */}
+        <HeroSliderFourteen />
+        {/* section title */}
+        <SectionTitleWithText spaceTopClass="pt-95" spaceBottomClass="pb-90" />
+        {/* tab product */}
+        <TabProductEight
+          spaceBottomClass="pb-70"
+          category="fashion"
+          sectionTitle={false}
+        />
+        {/* feature icon */}
+        <FeatureIconFour
+          bgImg="/assets/img/bg/shape.png"
+          containerClass="container-fluid"
+          gutterClass="padding-10-row-col"
+          spaceTopClass="pt-50"
+          spaceBottomClass="pb-40"
+        />
+
+        <TabProductTwelve
+          category="kids"
+          spaceTopClass="pt-95"
+          sectionTitle="Best Products"
+        />
+
+        <BannerNine spaceBottomClass="pb-70" />
+        <TabProductFive
+          spaceTopClass="pt-95"
+          spaceBottomClass="pb-70"
+          category="cosmetics"
+        />
+        <BannerFifteen spaceTopClass="pt-10" spaceBottomClass="pb-85" />
+
+        {/* newsletter */}
+        <NewsletterTwo spaceBottomClass="pb-100" />
+        <ProductGridFiveContainer
+          spaceTopClass="pt-100"
+          spaceBottomClass="pb-100"
+          category="accessories"
+        />
+        <BannerSixteen spaceTopClass="pt-95" />
+
+        {/* image slider */}
+        <ImageSliderOne />
+      </LayoutSeven>
+    </Fragment>
+  );
+};
+
+export default HomeFashionSix;
