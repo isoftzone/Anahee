cb44b6e4928470e916a21b401a16fb46f2f0b947
Sourabh <isoftsourabhb@gmail.com>
2025-01-21 18:10:21 +0530

    Update HomeFashionSix.js

--- src/pages/home/HomeFashionSix.js
+++ src/pages/home/HomeFashionSix.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import SEO from "../../components/seo";
 import LayoutSeven from "../../layouts/LayoutSeven";
 import HeroSliderFourteen from "../../wrappers/hero-slider/HeroSliderFourteen";
+import HeroSliderTen from "../../wrappers/hero-slider/HeroSliderTen";
 import SectionTitleWithText from "../../components/section-title/SectionTitleWithText";
 import FeatureIconFour from "../../wrappers/feature-icon/FeatureIconFour";
 import TabProductEight from "../../wrappers/product/TabProductEight";
@@ -12,11 +13,18 @@ import BannerNine from "../../wrappers/banner/BannerNine";
 import TabProductFive from "../../wrappers/product/TabProductFive";
 import BannerFifteen from "../../wrappers/banner/BannerFifteen";
 import BannerSixteen from "../../wrappers/banner/BannerSixteen";
-import ProductGridFiveContainer from "../../wrappers/product/ProductGridFiveContainer";
+import BannerEleven from "../../wrappers/banner/BannerEleven";
+// import ProductGridFiveContainer from "../../wrappers/product/ProductGridFiveContainer";
 import BannerTwentyEight from "../../wrappers/banner/BannerTwentyEight";
 import BannerSix from "../../wrappers/banner/BannerSix";
 import BannerSeven from "../../wrappers/banner/BannerSeven";
 import NewProductGrid from "../../wrappers/product/NewProductGrid";
+import ProductSliderThree from "../../wrappers/product/ProductSliderThree";
+import BannerTwo from "../../wrappers/banner/BannerTwo";
+import CategoryTwoSlider from "../../wrappers/category/CategoryTwoSlider";
+import BannerTwentySix from "../../wrappers/banner/BannerTwentySix";
+import BannerThirtyThree from "../../wrappers/banner/BannerThirtyThree";
+
 const HomeFashionSix = () => {
   return (
     <Fragment>
@@ -29,8 +37,10 @@ const HomeFashionSix = () => {
         <HeroSliderFourteen />
         {/* section title */}
         <SectionTitleWithText spaceTopClass="pt-95" spaceBottomClass="pb-90" />
+        <HeroSliderTen />
         {/* tab product */}
         <BannerTwentyEight spaceTopClass="pt-100" spaceBottomClass="pb-70" />
+      <CategoryTwoSlider spaceTopClass="pt-100" spaceBottomClass="pb-95" />
 
         <TabProductEight
           spaceBottomClass="pb-70"
@@ -51,10 +61,15 @@ const HomeFashionSix = () => {
           spaceTopClass="pt-95"
           sectionTitle="Best Products"
         />
+                <ProductSliderThree category="fashion" />
+                <BannerTwo spaceTopClass="pt-80" spaceBottomClass="pb-60" />
+        
+        <BannerTwentySix spaceBottomClass="pb-70" />
 
         <BannerSix spaceTopClass="pt-100" spaceBottomClass="pb-80" />
 
         <BannerNine spaceBottomClass="pb-70" />
+        <BannerEleven />
         <TabProductFive
           spaceTopClass="pt-95"
           spaceBottomClass="pb-70"
@@ -72,6 +87,7 @@ const HomeFashionSix = () => {
           category="accessories"
         /> */}
         <BannerSixteen spaceTopClass="pt-95" />
+        <BannerThirtyThree spaceBottomClass="pb-70" bgColorClass="bg-black-2" />
 
         {/* image slider */}
         <ImageSliderOne />
