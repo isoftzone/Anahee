334caba7d4c3680a1384a3b758880340d5b24b94
isoftzoneashish <isoftashishr@gmail.com>
2025-05-15 19:07:06 +0530

    maked responsive

--- web/src/wrappers/banner/ShopByCollection.js
+++ web/src/wrappers/banner/ShopByCollection.js
@@ -1,40 +1,74 @@
 import clsx from "clsx";
 
 const ShopByCollection = ({ spaceTopClass, spaceBottomClass }) => {
-  const isMobile = typeof window !== 'undefined' && window.innerWidth <= 768;
-
   return (
-    <div className={clsx("shop-by-collection px-5", spaceTopClass, spaceBottomClass)}>
-      <div className="container">
-        <div className={isMobile ? 'collection-section full-width' : 'collection-section'}>
-          <h2 className="title">Shop by Collection</h2>
-          <div className="underline"></div>
-          <div className="links">
-            <a href="#western">Western Wear</a>
+    <div
+      className={clsx("shop-by-collection", spaceTopClass, spaceBottomClass)}
+    >
+      <div className="container text-center">
+        <div className="collection-section mx-auto">
+          <h2 className="collection-title">Shop by Collection</h2>
+          <div className="underline mx-auto my-2"></div>
+          <div className="links mt-3">
+            <a href="#western" className="mx-2">
+              Western Wear
+            </a>
             <span className="divider">|</span>
-            <a href="#suit">Suit Sets</a>
+            <a href="#suit" className="mx-2">
+              Suit Sets
+            </a>
             <span className="divider">|</span>
-            <a href="#cord">Cord Sets</a>
+            <a href="#cord" className="mx-2">
+              Cord Sets
+            </a>
           </div>
         </div>
-      </div>
 
-      <div className="lookbook-container">
-        <div className="lookbook-carddd">
-          <div className="lookbook-cardd">
-            <img src="assets/img/banner/Maskgroup__11.png" alt="Lookbook 2025" className="lookbook-image" />
+        <div className="row justify-content-center mt-5 gx-4">
+          {/* Left Lookbook Section */}
+          <div className="col-lg-6 col-12 mb-4">
+            <div className="lookbook-main-container d-flex position-relative">
+              <div className="lookbook-image">
+                <img
+                  src="assets/img/banner/Maskgroup__11.png"
+                  alt="Lookbook 2025"
+                  className="img-fluid"
+                />
+              </div>
+              <div className="lookbook-text"></div>
+              <div className="lookbook-text-content d-flex flex-column justify-content-center align-items-start">
+                <div className="lookbook-year">2025</div>
+                <h2 className="lookbook-title">
+                  Lookbook
+                  <br />
+                  <span>Video</span>
+                </h2>
+                <a href="#" className="lookbook-link">
+                  SHOP NOW
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="overlay-content">
-            <div className="lookbook-year">2025</div>
-            <div className="lookbook-title">Lookbook<br /><span>Video</span></div>
-            <div className="shop-now">SHOP NOW</div>
+
+          {/* Right Top and Bottom Images */}
+          <div className="col-lg-3 col-sm-6 col-12 mb-4">
+            <div className="lookbook1">
+              <img
+                src="assets/img/banner/Maskgroup__22.png"
+                alt="Blue Dress"
+                className="img-fluid object-fit-cover"
+              />
+            </div>
           </div>
-        </div>
-        <div className="lookbook-card1">
-          <img src="assets/img/banner/Maskgroup__22.png" alt="Yellow Dress" className="lookbook-image" />
-        </div>
-        <div className="lookbook-card">
-          <img src="assets/img/banner/Maskgroup__33.png" alt="Blue Dress" className="lookbook-image" />
+          <div className="col-lg-3 col-sm-6 col-12 mb-4">
+            <div className="lookbook2">
+              <img
+                src="assets/img/banner/Maskgroup__33.png"
+                alt="Yellow Dress"
+                className="img-fluid  object-fit-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
