c38d1d719a58870bcce76823baa16bece7c3ddfe
isoftzoneashish <isoftashishr@gmail.com>
2025-05-17 20:21:09 +0530

    ch

--- web/src/wrappers/banner/PromoBanner.js
+++ web/src/wrappers/banner/PromoBanner.js
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { Link } from "react-router-dom";
-
 const PromoBanner = ({ spaceTopClass, spaceBottomClass }) => {
   return (
     <div className={clsx("promo-banner position-relative", spaceTopClass, spaceBottomClass)}>
@@ -8,9 +7,8 @@ const PromoBanner = ({ spaceTopClass, spaceBottomClass }) => {
       <img
         src="assets/img/banner/promo_banner.png"
         alt="Fashion Model"
-        className="promo-banner__model-img d-none d-lg-block"
+        className="promo-banner__model-img"
       />
-
       {/* First section - white background */}
       <div className="promo-banner__section promo-banner__section--top">
         <div className="container">
@@ -26,7 +24,6 @@ const PromoBanner = ({ spaceTopClass, spaceBottomClass }) => {
           </div>
         </div>
       </div>
-
       {/* Second section - light pink background */}
       <div className="promo-banner__section promo-banner__section--middle">
         <div className="container">
@@ -34,7 +31,7 @@ const PromoBanner = ({ spaceTopClass, spaceBottomClass }) => {
             <div className="col offset-lg-1">
               <h2 className="promo-banner__discount-text">
                 <p className="promo-banner__text">Get Set, Style!</p>
-                <span className="promo-banner__brand">Myntra </span>
+                <span className="promo-banner__brand">Anahee </span>
                 <span className="promo-banner__discount-phrase">
                   Discounts
                   <br />
@@ -49,60 +46,38 @@ const PromoBanner = ({ spaceTopClass, spaceBottomClass }) => {
           </div>
         </div>
       </div>
-
       {/* Third section - maroon background */}
       <div className="promo-banner__section promo-banner__section--bottom">
         <div className="container">
-          <div className="row">
-            <div className="col-12">
               <div className="row justify-content-center">
-                {/* Free Shipping */}
-                <div className="col-4 col-md-4 col-sm-4 promo-banner__feature">
-                  {/* <div className="promo-banner__icon text-white">🚚</div> */}
-                  <img className="promo-banner__icon" src="assets\img\logo\shipping.png" />
-                  <div>
-                    <h3 className="promo-banner__feature-title">
-                      Free Shipping
-                    </h3>
-                    <p className="promo-banner__feature-desc">
-                      A wonderful speed
-                    </p>
-                  </div>
-                </div>
-
-                {/* Secure Payment */}
-                <div className="col-4 col-md-4 col-sm-4 promo-banner__feature">
-                 <img className="promo-banner__icon" src="assets\img\logo\secure.png" />
-                  <div>
-                    {" "}
-                    <h3 className="promo-banner__feature-title">
-                      Secure Payment
-                    </h3>
-                    <p className="promo-banner__feature-desc">
-                      Feel safe for transactions
-                    </p>
-                  </div>
-                </div>
-
-                {/* Easy Returns */}
-                <div className="col-4 col-md-4 col-sm-4 promo-banner__feature mt-3 mt-md-0">
-                  <img className="promo-banner__icon" src="assets\img\logo\Group.png" />
-                  <div>
-                    <h3 className="promo-banner__feature-title">
-                      Easy Returns
-                    </h3>
-                    <p className="promo-banner__feature-desc">
-                      15 Days Return Policy
-                    </p>
-                  </div>
-                </div>
-              </div>
+  {/* Free Shipping */}
+  <div className="col-12 col-md-4 promo-banner__feature mb-4 mb-md-0">
+    <img className="promo-banner__icon img img-fluid" src="assets/img/logo/shipping.png" />
+    <div>
+      <h3 className="promo-banner__feature-title">Free Shipping</h3>
+      <p className="promo-banner__feature-desc">A wonderful speed</p>
+    </div>
+  </div>
+  {/* Secure Payment */}
+  <div className="col-12 col-md-4 promo-banner__feature mb-4 mb-md-0">
+    <img className="promo-banner__icon" src="assets/img/logo/secure.png" />
+    <div>
+      <h3 className="promo-banner__feature-title">Secure Payment</h3>
+      <p className="promo-banner__feature-desc">Feel safe for transactions</p>
+    </div>
+  </div>
+  {/* Easy Returns */}
+  <div className="col-12 col-md-4 promo-banner__feature">
+    <img className="promo-banner__icon" src="assets/img/logo/Group.png" />
+    <div>
+      <h3 className="promo-banner__feature-title">Easy Returns</h3>
+      <p className="promo-banner__feature-desc">15 Days Return Policy</p>
+    </div>
+  </div>
+</div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
   );
 };
-
 export default PromoBanner;
